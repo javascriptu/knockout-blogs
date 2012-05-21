536e12157765741ae96f1a920ced21ff6c86259a
Jess Carlos <jess@jesscarlos.com>
2012-05-21 01:28:24 -0700

    more files not needed

--- app.build.js
+++ app.build.js
@@ -6,7 +6,6 @@
   paths : {
     jquery       : 'vendor/jquery/jquery-1.7.2',
     knockout     : 'vendor/knockout/knockout-2.1.0',
-    template     : 'vendor/knockout/koExternalTemplateEngine.min',
     localstorage : "vendor/knockout/knockout.localStorage",
     moment       : "vendor/moment/moment-1.6.1",
 	sammy		 : "vendor/sammy/sammy-latest.min"
